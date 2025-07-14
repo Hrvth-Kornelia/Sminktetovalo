3e7f789e6498234d98df72b34834d535f76b24cd
Hrvth-Kornelia <hrvth.kornelia18@gmail.com>
2025-07-14 19:01:01 +0200

    Add files via upload

---
+++ sminktetovalo.js
@@ -0,0 +1,16 @@
+
+function copytoclipboard(elementID){
+    const text=document.getElementById(elementID).innerText;
+    navigator.clipboard.writeText(text).then(()=>{
+        alert('Másolva: ' + text);
+    
+    });
+}
+
+const css = document.createElement('link');
+  css.rel = 'stylesheet';
+  css.href = 'sminktetovalo.css?v=' + new Date().getTime(); // mindig új URL
+   css.href = 'sminktetovalo.portfolio.elolap.css?v=' + new Date().getTime(); // mindig új URL
+   css.href = 'sminktetovalo.arlista.css?v=' + new Date().getTime();
+   
+  document.head.appendChild(css);
